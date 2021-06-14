4df8630f993f9832f0fa980fa1b5453ccfd512ac
Avital Fine <79420960+AvitalFineRedis@users.noreply.github.com>
2021-06-14 15:24:11 +0300

    Examples to T-Digest functions (#39)
    * examples
    
    Co-authored-by: filipe oliveira <filipecosta.90@gmail.com>

--- example_client_test.go
+++ example_client_test.go
@@ -50,3 +50,144 @@ func ExampleNewClientFromPool() {
 	// Output: myItem exists in mytest:  true
 
 }
+
+// exemplifies the TdCreate function
+func ExampleClient_TdCreate() {
+	host := "localhost:6379"
+	var client = redisbloom.NewClient(host, "nohelp", nil)
+
+	ret, err := client.TdCreate("key", 100)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	fmt.Println(ret)
+	// Output: OK
+
+}
+
+// exemplifies the TdAdd function
+func ExampleClient_TdAdd() {
+	host := "localhost:6379"
+	var client = redisbloom.NewClient(host, "nohelp", nil)
+
+	key := "example"
+	ret, err := client.TdCreate(key, 100)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	samples := map[float64]float64{1.0: 1.0, 2.0: 2.0}
+	ret, err = client.TdAdd(key, samples)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	fmt.Println(ret)
+	// Output: OK
+
+}
+
+// exemplifies the TdMin function
+func ExampleClient_TdMin() {
+	host := "localhost:6379"
+	var client = redisbloom.NewClient(host, "nohelp", nil)
+
+	key := "example"
+	_, err := client.TdCreate(key, 10)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	samples := map[float64]float64{1.0: 1.0, 2.0: 2.0, 3.0: 3.0}
+	_, err = client.TdAdd(key, samples)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	min, err := client.TdMin(key)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	fmt.Println(min)
+	// Output: 1
+}
+
+// exemplifies the TdMax function
+func ExampleClient_TdMax() {
+	host := "localhost:6379"
+	var client = redisbloom.NewClient(host, "nohelp", nil)
+
+	key := "example"
+	_, err := client.TdCreate(key, 10)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	samples := map[float64]float64{1.0: 1.0, 2.0: 2.0, 3.0: 3.0}
+	_, err = client.TdAdd(key, samples)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	max, err := client.TdMax(key)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	fmt.Println(max)
+	// Output: 3
+}
+
+// exemplifies the TdQuantile function
+func ExampleClient_TdQuantile() {
+	host := "localhost:6379"
+	var client = redisbloom.NewClient(host, "nohelp", nil)
+
+	key := "example"
+	_, err := client.TdCreate(key, 10)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	samples := map[float64]float64{1.0: 1.0, 2.0: 1.0, 3.0: 1.0, 4.0: 1.0, 5.0: 1.0}
+	_, err = client.TdAdd(key, samples)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	ans, err := client.TdQuantile(key, 1.0)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	fmt.Println(ans)
+	// Output: 5
+}
+
+// exemplifies the TdCdf function
+func ExampleClient_TdCdf() {
+	host := "localhost:6379"
+	var client = redisbloom.NewClient(host, "nohelp", nil)
+
+	key := "example"
+	_, err := client.TdCreate(key, 10)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	samples := map[float64]float64{1.0: 1.0, 2.0: 1.0, 3.0: 1.0, 4.0: 1.0, 5.0: 1.0}
+	_, err = client.TdAdd(key, samples)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	cdf, err := client.TdCdf(key, 1.0)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	fmt.Println(cdf)
+	// Output: 0.1
+}
